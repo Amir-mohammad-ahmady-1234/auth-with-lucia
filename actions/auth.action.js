d3e04f05e0c7858f075b8dcaf37c0481ac22dc8e
Amir-Mohammad <amyrmhmdahmdy179@gmail.com>
2025-08-24 12:55:34 -0700

    create prject + added auth-action

---
+++ actions/auth.action.js
@@ -0,0 +1,21 @@
+"use server";
+
+export async function singup(prevState, formData) {
+  const email = formData.get("email");
+  const password = formData.get("password");
+
+  const errors = {};
+
+  // validations
+  if (!email.includes("@")) {
+    errors.email = "invalid email, you should use @ in your email";
+  }
+  if (password.trim().length < 8) {
+    errors.password = "password can't less than 8 characters";
+  }
+  if (Object.keys(errors).length > 0) {
+    return { errors };
+  }
+
+  // store it data in database and create new user
+}
